cdbe9b35df45e8642bfad756b7982303fb1a8595
revouweek10 <143944574+revouweek10@users.noreply.github.com>
2023-09-03 21:22:47 +0700

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 
-const MONGO_PROD = `mongodb://mongo:********@containers-us-west-154.railway.app:7090`
+const MONGO_PROD = `mongodb://mongo:oC03pBOSTy51ELIt3hG9@containers-us-west-188.railway.app:7567`
 const MONGO_URI= `mongodb://127.0.0.1:27017`
 
 const DB_URI = process.env.ENV === 'production' ? MONGO_PROD : MONGO_URI 
@@ -8,7 +8,7 @@ const connectToDb = async () => {
   try {
     const client = await new MongoClient(DB_URI).connect();
     const db = client.db(process.env.MONGO_DB);
-    console.log(`You have been CONNECTED.. `);
+    console.log(`You have been CONNECTED.. lets goo...!!! `);
     return db
   } catch (error) {
     console.log(error);
